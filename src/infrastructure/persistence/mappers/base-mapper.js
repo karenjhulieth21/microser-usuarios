de9b3b13256884f188ae1a6c1f43a4f663f53cff
karenjhulieth21 <karen.jhulieth.lucumi@correounivalle.edu.co>
2026-04-15 22:22:56 -0500

    actualizacion dos

--- src/infrastructure/persistence/mappers/base-mapper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Mapper = void 0;
-var Mapper = /** @class */ (function () {
-    function Mapper() {
-    }
-    return Mapper;
-}());
-exports.Mapper = Mapper;
